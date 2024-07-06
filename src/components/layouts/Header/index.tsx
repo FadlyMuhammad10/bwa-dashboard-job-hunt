b71b9ba36eddbcf2f9df9b55d162c4867a0504b1
Muhammad Fadly <davit9747@gmail.com>
2024-07-06 11:39:29 +0700

    slicing post job page 1 & 2

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -1,18 +1,22 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { FunctionComponent } from "react";
 
 interface HeaderProps {}
 
 const Header: FunctionComponent<HeaderProps> = () => {
+  const { push } = useRouter();
+  const navCreateJobPage = () => push("/post-a-job");
   return (
-    <div className="pb-3 mb-8 border-border flex flex-row items-center justify-between">
+    <div className="pb-3 mb-8 border-b border-border flex flex-row items-center justify-between">
       <div>
         <div>Company</div>
         <div className="font-semibold">Twitter</div>
       </div>
       <div>
-        <Button className="rounded-none py-3 px-6">
+        <Button onClick={navCreateJobPage} className="rounded py-3 px-6">
           <PlusIcon className="mr-2 w-4 h-4" />
           Post a Job
         </Button>
---
+++ src/lib/form-schema.ts
@@ -0,0 +1,43 @@
+"use client";
+
+import { JOBTYPES } from "@/constants";
+import { z } from "zod";
+
+export const jobFormSchema = z.object({
+  roles: z
+    .string({
+      required_error: "Job title is required",
+    })
+    .min(3, { message: "Job title must be at least 3 characters" }),
+  jobType: z.enum(JOBTYPES, { required_error: "You must select a job type" }),
+  salaryFrom: z.string({
+    required_error: "Salary From is required",
+  }),
+  salaryTo: z.string({
+    required_error: "Salary To is required",
+  }),
+  categoryId: z.string({ required_error: "Category is required selected" }),
+  requiredSkills: z
+    .string()
+    .array()
+    .nonempty({ message: "Required skills must be 1 selected" }),
+  JobDescription: z
+    .string({ required_error: "Job Description is required" })
+    .min(10, { message: "Job Description must be at least 10 characters" }),
+  responsibility: z
+    .string({ required_error: "Job Description is required" })
+    .min(10, { message: "Job Description must be at least 10 characters" }),
+  whoYouAre: z
+    .string({ required_error: "Job Description is required" })
+    .min(10, { message: "Job Description must be at least 10 characters" }),
+  niceToHaves: z
+    .string({ required_error: "Job Description is required" })
+    .min(10, { message: "Job Description must be at least 10 characters" }),
+  benefits: z
+    .object({
+      Benefit: z.string(),
+      description: z.string(),
+    })
+    .array()
+    .nonempty({ message: "Benefits must be 1 selected" }),
+});
